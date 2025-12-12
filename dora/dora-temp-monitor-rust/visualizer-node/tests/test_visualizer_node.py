4302d9cfa70c4299c5d9c53d04ecce2288982cbe
doomking <xghan.cn@gmail.com>
2025-12-12 14:58:06 +0800

    add dora demo temperature monitor

---
+++ dora/dora-temp-monitor-rust/visualizer-node/tests/test_visualizer_node.py
@@ -0,0 +1,13 @@
+"""Test module for visualizer_node package."""
+
+import pytest
+
+
+def test_import_main():
+    """Test importing and running the main function."""
+    from visualizer_node.main import main
+
+    # Check that everything is working, and catch Dora RuntimeError
+    # as we're not running in a Dora dataflow.
+    with pytest.raises(RuntimeError):
+        main()
